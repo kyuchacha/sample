463aaa64546bdfc15475618e52f59545341edd71
USER <USER@parksunghoon-pc>
2022-04-15 17:56:03 +0900

    ee

--- testGit/src/main/java/com/example/demo/TestGitApplication.java
+++ testGit/src/main/java/com/example/demo/TestGitApplication.java
@@ -1,13 +1,28 @@
 package com.example.demo;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootApplication
-public class TestGitApplication {
+public class TestGitApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestGitApplication.class, args);
 	}
 
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(TestGitApplication.class);
+
+	}
+
+	
 }
--- testGit/src/main/java/com/example/demo/test/testController.java
+++ testGit/src/main/java/com/example/demo/test/testController.java
@@ -1,11 +1,17 @@
 package com.example.demo.test;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class testController {
 
+	@GetMapping("/")
+	public String index() {
+		System.out.println("????????????");
+		return "index";
+	}
 	@GetMapping("/hello")
 	public String hello() {
 		return "hello!!!";
