bb8b4fe5087d17c462610059b0bdb3e010a35ae2
USER <USER@parksunghoon-pc>
2022-04-19 16:22:02 +0900

    cmd

--- testGit/test_deploy.cmd
+++ testGit/test_deploy.cmd
@@ -1,4 +1,4 @@
-echo "===========start==========="
+echo "===========jeusadmin connect==========="
 jeusadmin -u administrator -p 123456789! 
 echo "===========install==========="
 install-application -id sample D:\Jenkins\workspace\mavenTest\testGit\target\testGit-0.0.1-SNAPSHOT.war
