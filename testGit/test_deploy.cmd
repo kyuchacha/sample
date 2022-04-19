9f2f607ec1546dc897b1d9510a35cef1d7421756
USER <USER@parksunghoon-pc>
2022-04-19 18:44:48 +0900

    gggg

--- testGit/test_deploy.cmd
+++
@@ -1,6 +0,0 @@
-echo "===========jeusadmin connect==========="
-jeusadmin -u administrator -p 123456789! 
-echo "===========install==========="
-install-application -id sample D:\Jenkins\workspace\mavenTest\testGit\target\testGit-0.0.1-SNAPSHOT.war
-echo "===========deploy==========="
-deploy sample -servers server1 -contextPath /sample
